5bfef325f87f143e1f43a9cc52df107a4b81bae8
Jose Juan Perez Gonzalez <pepeju95@gmail.com>
2021-05-11 15:34:17 +0200

    refactor auth y teams

---
+++ teams/teams.http.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+const teamsController = require("./teams.controller");
+const { getUser } = require("../auth/users.controller");
+
+const getTeamFromUser = (req, res) => {
+    let user = getUser(req.user.userId);
+    res.status(200).json({
+        trainer: user.userName,
+        team: teamsController.getTeamOfUser(req.user.userId)
+    })
+}
+
+const setTeamToUser = (req, res) => {
+    teamsController.setTeam(req.user.userId, req.body.team);
+    res.status(200).send();
+}
+
+const addPokemonToTeam = (req, res) => {
+    let pokemonName = req.body.name;
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
+        .then(function (response) {
+            let pokemon = {
+                name: pokemonName,
+                pokedexNumber: response.data.id
+            };
+            teamsController.addPokemon(req.user.userId, pokemon);
+            
+            res.status(201).json(pokemon);
+        })
+        .catch(function (error) {
+            console.log(error);
+            res.status(400).json({message: error});
+        })
+        .then(function () {
+
+        });
+}
+
+const deletePokemonFromTeam = (req,res) => {
+        teamsController.deletePokemon(req.user.userId, req.params.pokeid);
+        res.status(200).send();
+}
+
+exports.getTeamFromUser = getTeamFromUser;
+exports.setTeamToUser = setTeamToUser;
+exports.addPokemonToTeam = addPokemonToTeam;
+exports.deletePokemonFromTeam = deletePokemonFromTeam;
--- teams/teams.router.js
+++ teams/teams.router.js
@@ -1,51 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
 
-const teamsController = require("./teams.controller");
-const { getUser } = require("../auth/users.controller");
+const teamsHttpHandler = require("./teams.http");
 
 router.route("/")
-    .get((req, res) => {
-        let user = getUser(req.user.userId);
-        res.status(200).json({
-            trainer: user.userName,
-            team: teamsController.getTeamOfUser(req.user.userId)
-        })
-    })
-    .put((req, res) => {
-        teamsController.setTeam(req.user.userId, req.body.team);
-        res.status(200).send();
-    })
+    .get(teamsHttpHandler.getTeamFromUser)
+    .put(teamsHttpHandler.setTeamToUser)
 
 router.route("/pokemons")
-    .post((req, res) => {
-        let pokemonName = req.body.name;
-        console.log("Calling pokeApi");
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
-            .then(function (response) {
-                let pokemon = {
-                    name: pokemonName,
-                    pokedexNumber: response.data.id
-                };
-                teamsController.addPokemon(req.user.userId, pokemon);
-                
-                res.status(201).json(pokemon);
-            })
-            .catch(function (error) {
-                console.log(error);
-                res.status(400).json({message: error});
-            })
-            .then(function () {
-
-            });
-    })
+    .post(teamsHttpHandler.addPokemonToTeam)
 
 router.route("/pokemons/:pokeid")
-    .delete((req,res) => {
-        teamsController.deletePokemon(req.user.userId, req.params.pokeid);
-        res.status(200).send();
-    })
+    .delete(teamsHttpHandler.deletePokemonFromTeam)
 
     
 exports.router = router;
